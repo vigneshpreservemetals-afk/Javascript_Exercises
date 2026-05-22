fbbcbb5c2db50cc0fb388ae16fbec752475988ee
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-22 23:15:05 +0530

    added new sample files for my references

---
+++ ExercisesForMyReference/BasicProduct.js
@@ -0,0 +1,15 @@
+
+
+// Basic Example - Product List
+
+let ProductName =        "Tshirt";
+let PriceOftheProducts = 300;
+let Tax                = 18;
+
+let TotalPrice = (PriceOftheProducts + Tax/100);
+let ProductDetails = `${ProductName} has been purchased from Zudio at the cost of ${300}`;
+
+
+console.log("ProductName===>",ProductName)
+console.log("TotalPrice===>",TotalPrice)
+console.log("ProductDetails===>",ProductDetails)
