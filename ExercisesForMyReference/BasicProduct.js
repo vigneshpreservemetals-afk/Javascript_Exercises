eccb7fc371f2c83eb63317db9a89ecbaaa06c35f
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-23 09:33:50 +0530

    added new files

--- ExercisesForMyReference/BasicProduct.js
+++ ExercisesForMyReference/BasicProduct.js
@@ -1,6 +1,6 @@
 
 
-// Basic Example - Product List
+// Basic Example 1 - Product List
 
 let ProductName =        "Tshirt";
 let PriceOftheProducts = 300;
@@ -13,3 +13,19 @@ let ProductDetails = `${ProductName} has been purchased from Zudio at the cost o
 console.log("ProductName===>",ProductName)
 console.log("TotalPrice===>",TotalPrice)
 console.log("ProductDetails===>",ProductDetails)
+
+
+// Basic Example 2 - Product List
+
+let FruitName = "Guava";
+let Count     = 20;
+let Price     = 15;
+
+let FinalAmount = (Price * Count);
+let FruitsDetails = `${FruitName} has been purchased from market at the cost of Rs${TotalPrice}`;
+
+
+console.log("FruitName===>",FruitName)
+console.log("FinalAmount===>",FinalAmount)
+console.log("FruitsDetails===>",FruitsDetails)
+
