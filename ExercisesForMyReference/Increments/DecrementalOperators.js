4209bfcd2ce616499cf9707dc693a8792e714943
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-23 09:34:40 +0530

    added incremental/decremental

---
+++ ExercisesForMyReference/Increments/DecrementalOperators.js
@@ -0,0 +1,46 @@
+
+//Example 1: Post Increment
+
+var a =  20;
+a++
+var b = a++
+
+console.log(a)
+console.log("PostIncrement===>",b)
+
+console.log("--------------------------------------------------------")
+
+
+//Example 2: Post Decrement
+
+var c =  20;
+c--
+var d = c++
+
+console.log(c)
+console.log("PostDecrement===>",d)
+
+console.log("--------------------------------------------------------")
+
+
+//Example 3: Pre Decrement
+
+var e =  20;
+--e
+var f = --e
+
+console.log(e)
+console.log("PreDecrement===>",f)
+
+
+console.log("--------------------------------------------------------")
+
+
+//Example 4: Pre Increment
+
+var g =  20;
+--g
+var h = --g
+
+console.log(g)
+console.log("PreIncrement===>",h)
